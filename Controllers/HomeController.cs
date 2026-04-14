4774ddbda7eb29b4deeea28bef48e523bb2a3072
Arjya Biswal <abiswal430@gmail.com>
2026-04-14 18:14:17 +0530

    Minor UI improvement

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using ExclusiveMVC.Models;
 
 namespace ExclusiveMVC.Controllers;
 
+// Final UI improvement for submission
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
